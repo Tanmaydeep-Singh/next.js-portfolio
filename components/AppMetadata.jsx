d31f6742205683661ce0d4de5d01d7d8c93a9979
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2023-06-24 18:49:00 +0530

    dev: code pushed

---
+++ components/AppMetadata.jsx
@@ -0,0 +1,26 @@
+const author = "Tanmaydeep Singh";
+const description =
+	"Software developer from Moldova, Republic of, who loves to develop beautiful websites, single page applications, customer relationship management or code from scratch using React and Next.js";
+const url = "https://tanmaydeep-singh.netlify.app";
+export const AppMetadata = {
+	metadataBase: new URL("https://tanmaydeep-singh.netlify.app/"),
+	title: {
+		default: `Portfolio | ${author}`,
+		template: `%s | ${author}`
+	},
+	description: description,
+	icons: {
+		icon: "/favicon.png"
+	},
+	creator: author,
+	authors: [{ name: author, url: url }],
+	colorScheme: "dark",
+	openGraph: {
+		title: `${author} | Portfolio`,
+		description: description,
+		url: url,
+		siteName: `${author} | Portfolio`,
+		locale: "en-IN",
+		type: "website"
+	}
+};
